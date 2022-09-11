2dcd8abfa1a2bcc50cc94772505c4bb1c0db3078
Vurnex <hamilton.laquon@gmail.com>
2022-09-11 16:16:00 -0500

    Add modal for new books
    Also added some initial javascript for creating books.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+}
+
+const nextBook = new Book('New Title', 'New Author', 100, false);
+
+
+function addBooksToLibrary() {
+
+
+}
