951bf2f978392337a8087331fa34b6f4cd6b1437
Vurnex <hamilton.laquon@gmail.com>
2022-09-15 12:43:39 -0500

    Allow submit button to create new books/cards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let myLibrary = [];
 
+let cardContainer = document.getElementById("container-main");
+
 document.getElementById('submit-btn').addEventListener("click", function() {
 
     const newTitle = document.getElementById('bookTitle').value;
@@ -11,11 +13,13 @@ document.getElementById('submit-btn').addEventListener("click", function() {
 
     const nextBook = new Book(newTitle, newAuthor, newPageNum, newReadStatus);
 
-    console.log(nextBook);
+    //console.log(nextBook);
+
+    addCards();
 
     addBooksToLibrary(nextBook);
 
- });
+});
 
 function Book(title, author, pages, read) {
 
@@ -31,4 +35,60 @@ function addBooksToLibrary(nextBook) {
     myLibrary.push(nextBook);
 
     console.table(myLibrary);
+}
+
+function addCards() {
+
+    let firstElement = document.createElement('div');
+    let secondElement = document.createElement('div');
+
+    let header = document.createElement('h5');
+    let firstPara = document.createElement('p');
+    let secondPara = document.createElement('p');
+    let thirdPara = document.createElement('p');
+
+    let buttonContainer = document.createElement('div');
+
+    let firstButton = document.createElement('a');
+    let secondButton = document.createElement('a');
+
+    //Add child elements to card
+
+    cardContainer.appendChild(firstElement);
+
+    firstElement.classList.add("card", "text-center", "shadow");
+    firstElement.appendChild(secondElement);
+
+    secondElement.classList.add("card-body");
+    secondElement.appendChild(header);
+    secondElement.appendChild(firstPara);
+    secondElement.appendChild(secondPara);
+    secondElement.appendChild(thirdPara);
+
+    secondElement.appendChild(buttonContainer);
+
+    buttonContainer.appendChild(firstButton);
+    buttonContainer.appendChild(secondButton);
+
+    header.classList.add("card-title");
+    header.textContent = "Book Title";
+
+    firstPara.classList.add("card-text");
+    firstPara.textContent = "Book Author";
+
+    secondPara.classList.add("card-text");
+    secondPara.textContent = "Some quick example text to build on the card title and make up the bulk of the card's content.";
+
+    thirdPara.textContent = "Page Number";
+
+    buttonContainer.classList.add("btn-container");
+
+    firstButton.classList.add("btn", "btn-primary");
+    firstButton.setAttribute("href", "#");
+    firstButton.textContent = "Read (x)";
+
+    secondButton.classList.add("btn", "btn-primary");
+    secondButton.setAttribute("href", "#");
+    secondButton.textContent = "Remove";
+
 }
