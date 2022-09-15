505abbf202aec3968c91a435d39aa99de2527006
Vurnex <hamilton.laquon@gmail.com>
2022-09-15 16:28:02 -0500

    Allow user to populate cards with data

--- js/script.js
+++ js/script.js
@@ -6,25 +6,25 @@ document.getElementById('submit-btn').addEventListener("click", function() {
 
     const newTitle = document.getElementById('bookTitle').value;
     const newAuthor = document.getElementById('bookAuthor').value;
+    const newBkDesc = document.getElementById('bookDesc').value;
     const newPageNum = document.getElementById('pageNum').value;
     const newReadStatus = document.getElementById('flexSwitchCheckDefault').checked;
 
     //console.log(newTitle + "\n" + newAuthor + "\n" + newPageNum + "\n" + newReadStatus)
 
-    const nextBook = new Book(newTitle, newAuthor, newPageNum, newReadStatus);
+    const nextBook = new Book(newTitle, newAuthor, newBkDesc, newPageNum, newReadStatus);
 
     //console.log(nextBook);
-
-    addCards();
 
     addBooksToLibrary(nextBook);
 
 });
 
-function Book(title, author, pages, read) {
+function Book(title, author, description, pages, read) {
 
     this.title = title;
     this.author = author;
+    this.description = description;
     this.pages = pages;
     this.read = read;
 
@@ -34,10 +34,17 @@ function addBooksToLibrary(nextBook) {
 
     myLibrary.push(nextBook);
 
-    console.table(myLibrary);
+    const updateDisplay = document.getElementById('container-main');
+    const selectCards = document.querySelectorAll('.card');
+    selectCards.forEach(book => updateDisplay.removeChild(book));
+
+    for (let i=0; i<myLibrary.length; i++){
+        addCards(myLibrary[i]);
+    }
+
 }
 
-function addCards() {
+function addCards(newBook) {
 
     let firstElement = document.createElement('div');
     let secondElement = document.createElement('div');
@@ -51,6 +58,8 @@ function addCards() {
 
     let firstButton = document.createElement('a');
     let secondButton = document.createElement('a');
+
+    const getReadStatus = document.getElementById('flexSwitchCheckDefault').checked;
 
     //Add child elements to card
 
@@ -71,15 +80,10 @@ function addCards() {
     buttonContainer.appendChild(secondButton);
 
     header.classList.add("card-title");
-    header.textContent = "Book Title";
 
     firstPara.classList.add("card-text");
-    firstPara.textContent = "Book Author";
 
     secondPara.classList.add("card-text");
-    secondPara.textContent = "Some quick example text to build on the card title and make up the bulk of the card's content.";
-
-    thirdPara.textContent = "Page Number";
 
     buttonContainer.classList.add("btn-container");
 
@@ -90,5 +94,15 @@ function addCards() {
     secondButton.classList.add("btn", "btn-primary");
     secondButton.setAttribute("href", "#");
     secondButton.textContent = "Remove";
+
+    header.textContent = newBook.title;
+    firstPara.textContent = newBook.author;
+    secondPara.textContent = newBook.description;
+    thirdPara.textContent = newBook.pages;
+
+    if (getReadStatus == true) {
+
+        firstButton.textContent = "Read (Y)";
+    }
 
 }
